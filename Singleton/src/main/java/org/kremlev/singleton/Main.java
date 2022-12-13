912d030b65a58a60b2a85210167a9c6747cf47d9
kremlevmax <kremlevmax.webdev@icloud.com>
2022-12-12 23:49:34 -0800

    Comment

--- Singleton/src/main/java/org/kremlev/singleton/Main.java
+++ Singleton/src/main/java/org/kremlev/singleton/Main.java
@@ -2,6 +2,7 @@ package org.kremlev.singleton;
 
 public class Main {
     public static void main(String[] args) {
+        // .createInstance() returns static instance of Singleton class which is equal to object or null.
         Singleton mySingleton = Singleton.createInstance();
         mySingleton.printMessage();
     }
