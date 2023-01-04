c238edeb7b1b192c9b83aade7e92126b59b5e016
kremlevmax <kremlevmax.webdev@icloud.com>
2023-01-03 21:48:29 -0800

    Spring Scope Singleton and Prototyp

--- MusicPlayer/src/main/java/org/maxkremlev/Main.java
+++ MusicPlayer/src/main/java/org/maxkremlev/Main.java
@@ -1,13 +1,23 @@
 package org.maxkremlev;
 
+import org.maxkremlev.Music.CountryMusic;
+import org.maxkremlev.Music.Music;
+import org.maxkremlev.Music.RockMusic;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        Music bean1 = context.getBean("rockMusic", RockMusic.class);
+        Music bean2 = context.getBean("rockMusic", RockMusic.class);
+        System.out.println(bean1);
+        System.out.println(bean2);
 
-        musicPlayer.playMusic();
+        Music bean3 = context.getBean("countryMusic", CountryMusic.class);
+        Music bean4 = context.getBean("countryMusic", CountryMusic.class);
+        System.out.println(bean3);
+        System.out.println(bean4);
+
     }
 }
